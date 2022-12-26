c6b3b8d5b3dea13c994ed61e1afb4ebdc109c8f7
Beshir Saeed <Beshir-saeed@live.com>
2022-12-26 10:05:54 +0300

    implement API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import logo from "./logo.svg";
-import Carousel from "./components/Carousel";
+import CarouselComponent from "./components/CarouselComponent";
 import "./App.css";
-import Button from "react-bootstrap/Button";
 
 const App = () => {
-	const [count, setCount] = useState(0);
+	const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
 
 	return (
 		<div className="App">
-			<header className="App-content">
+			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<h1>NASA Astronomy of the Day</h1>
 				<form className="">
-					<input type={"date"}></input>
-					<button type="submit" className="bg-black">
+					<input id="dateInput" type={"date"}></input>
+					<button
+						onClick={(e) => setDate(e.target.dateInput.value)}
+						type="button"
+						className="bg-black"
+					>
 						Check it out
 					</button>
 				</form>
@@ -23,9 +26,10 @@ const App = () => {
 						count is: {count}
 					</button>
 				</p> */}
-
-				<Carousel></Carousel>
 			</header>
+			<div className="App-content">
+				<CarouselComponent date></CarouselComponent>
+			</div>
 		</div>
 	);
 };
